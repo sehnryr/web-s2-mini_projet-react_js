2ce56cdb6213ec024e58efdcedccb6189db04ed1
Nano <nanoman2212@gmail.com>
2021-05-11 10:01:27 +0200

    begin of text implementation for presentation

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -13,6 +13,7 @@ export default function Presentation(props) {
 	return (
 		<div className="section">
 			<h1 className={classes.root} >{props.title}</h1>
+			<p>{props.content}</p>
 		</div>
 	)
 }
